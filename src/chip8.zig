7e940ffb157f5a741cb4a1d5cda57880a5768028
floorpigeon <77025454+floorpigeon@users.noreply.github.com>
2026-06-10 13:22:16 +1000

    fixed sdl dependency

--- src/chip8.zig
+++ src/chip8.zig
@@ -1,4 +1,8 @@
 const std = @import("std");
+const c = @import("c");
+
+const Self = @This();
+
 const fontset = [80]u8{
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -17,159 +21,180 @@ const fontset = [80]u8{
     0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
     0xF0, 0x80, 0xF0, 0x80, 0x80, // F
 };
-const Chip8 = struct {
-    const Self = @This();
 
-    opcode: u16 = 0,
-    memory: [4096]u8 = fontset ++ @as([4096 - fontset.len]u8, @splat(0)),
-    V: [16]u8 = 0,
-    I: u16 = 0,
-    pc: u16 = 0x200,
-    gfx: [64 * 32]u8 = @splat(0),
-    delay_timer: u8 = 0,
-    sound_timer: u8 = 0,
-    stack: [16]u16 = @splat(0),
-    sp: u16 = 0,
-    key: [16]u8 = 0,
+opcode: u16 = 0,
+memory: [4096]u8 = fontset ++ @as([4096 - fontset.len]u8, @splat(0)),
+V: [16]u8 = @splat(0),
+I: u16 = 0,
+pc: u16 = 0x200,
+gfx: [64 * 32]u8 = @splat(0),
+delay_timer: u8 = 0,
+sound_timer: u8 = 0,
+stack: [16]u16 = @splat(0),
+sp: u16 = 0,
+key: [16]u8 = @splat(0),
 
-    pub fn load_game(self: *Self, name: []const u8) !void {
-        const file = try std.fs.cwd().openFile(name, .{});
-        defer file.close;
-        _ = try file.read(self.memory[0x200..]);
-    }
-    pub fn emulate_cycle(self: *Self) void {
-        // chip8 opcodes are 2 bytes so need to offset then bit mask
-        self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc + 1];
-        std.debug.print("0x{x}\n", .{self.opcode});
+pub fn init() Self {
+    std.debug.print("You initialised chip8\n", .{});
+    return .{};
+}
+pub fn load_game(self: *Self, name: []const u8) !void {
+    const file = try std.fs.cwd().openFile(name, .{});
+    defer file.close;
+    _ = try file.readAll(self.memory[0x200..]);
+}
+pub fn emulate_cycle(self: *Self) void {
+    // chip8 opcodes are 2 bytes so need to offset then bit mask
+    self.opcode = self.memory[self.pc] << 8 | self.memory[self.pc + 1];
+    std.debug.print("0x{x}\n", .{self.opcode});
 
-        // Isolate the opcode fields so we can use them individually
-        const x: u4 = (self.opcode & 0x0F00) >> 8;
-        const y: u4 = (self.opcode & 0x00F0) >> 4;
-        switch (self.opcode & 0xF000) {
-            0x0000 => switch (self.opcode & 0x00FF) {
-                // Clear screen
-                0x00E0 => {
-                    @memset(self.gfx, 0);
-                    self.pc += 2;
-                },
-                0x00EE => {
-                    self.sp -= 1;
-                    self.pc = self.stack[self.sp];
-                    self.pc += 2;
-                },
-            },
-            // 1NNN
-            // Jump to address NNN
-            0x1000 => self.pc = self.opcode & 0x0FFF,
-            // 2NNN
-            0x2000 => {
-                self.stack[self.sp] = self.pc;
-                self.sp += 1;
-                self.pc = self.opcode & 0x0FFF;
-            },
-            // 3XNN
-            // Skip next instruction if the value in VX == NN
-            0x3000 => {
-                if (self.V[(0x0F00 & self.opcode) >> 8] == (0x00FF & self.opcode)) {
-                    self.pc += 4;
-                } else {
-                    self.pc += 2;
-                }
-            },
-            // 4XNN
-            // Skip next instruction if the value in VX != NN
-            0x4000 => {
-                if (self.V[(0x0F00 & self.opcode) >> 8] != (0x00FF & self.opcode)) {
-                    self.pc += 4;
-                } else {
-                    self.pc += 2;
-                }
-            },
-            // 5XY0
-            // Skip next instruction if registers are equal
-            0x5000 => {
-                if (self.V[(0x0F00 & self.opcode) >> 8] == self.V[(0x0F00 & self.opcode) >> 4]) {
-                    self.pc += 4;
-                } else {
-                    self.pc += 2;
-                }
-            },
-            // 6XNN
-            // Set register X to number NN
-            0x6000 => {
-                self.V[x] = self.opcode & 0x00FF;
+    // Isolate the opcode fields so we can use them individually
+    const x: u4 = (self.opcode & 0x0F00) >> 8;
+    const y: u4 = (self.opcode & 0x00F0) >> 4;
+    switch (self.opcode & 0xF000) {
+        0x0000 => switch (self.opcode & 0x00FF) {
+            // Clear screen
+            0x00E0 => {
+                @memset(&self.gfx, 0);
                 self.pc += 2;
             },
-            // 7XNN
-            // Add number NN to register X
-            0x7000 => {
-                self.V[x] += self.opcode & 0x00FF;
+            0x00EE => {
+                self.sp -= 1;
+                self.pc = self.stack[self.sp];
                 self.pc += 2;
             },
-            0x8000 => {
-                // switch statement
-            },
-            // 9XY0
-            // Skip next instruction if registers are not equal
-            0x9000 => {
-                if (self.V[(0x0F00 & self.opcode) >> 8] != self.V[(0x0F00 & self.opcode) >> 4]) {
-                    self.pc += 4;
-                } else {
-                    self.pc += 2;
-                }
-            },
-            // ANNN
-            // Add NNN at ANNN to I register
-            0xA000 => {
-                self.I = self.opcode & 0x0FFF;
+        },
+        // 1NNN
+        // Jump to address NNN
+        0x1000 => self.pc = self.opcode & 0x0FFF,
+        // 2NNN
+        0x2000 => {
+            self.stack[self.sp] = self.pc;
+            self.sp += 1;
+            self.pc = self.opcode & 0x0FFF;
+        },
+        // 3XNN
+        // Skip next instruction if the value in VX == NN
+        0x3000 => {
+            if (self.V[(0x0F00 & self.opcode) >> 8] == (0x00FF & self.opcode)) {
+                self.pc += 4;
+            } else {
                 self.pc += 2;
-            },
-            // Unimplemented
-            0xB000 => {
-                // Something idk
-            },
-            // CXNN
-            // Random
-            0xC000 => {
-                // self.V[x] = (self.opcode & 0x00FF) & (random something zig)
-            },
-            // DXYN
-            // Display
-            0xD000 => {
-                // Grab the coordinates in registers X and Y
-                const xPos: usize = self.V[x];
-                const yPos: usize = self.V[y];
-                // Set VF to 0
-                self.V[0xF] = 0;
-                // For N rows
-                for (0..(self.opcode & 0x000F)) |row| {
-                    // Get the Nth (row) byte of sprite data
-                    const sprite_row = self.memory[self.I + row];
-                    // 0x80 is 10000000 which is the first in the sprite row
-                    for (0..8) |bit| {
-                        // Don't know if the intcast thing is necessary TODO test
-                        if (sprite_row & (0x80 >> @intCast(bit)) != 0) {
-                            const px = (xPos + bit) % 64;
-                            const py = (yPos + row) % 32;
+            }
+        },
+        // 4XNN
+        // Skip next instruction if the value in VX != NN
+        0x4000 => {
+            if (self.V[(0x0F00 & self.opcode) >> 8] != (0x00FF & self.opcode)) {
+                self.pc += 4;
+            } else {
+                self.pc += 2;
+            }
+        },
+        // 5XY0
+        // Skip next instruction if registers are equal
+        0x5000 => {
+            if (self.V[(0x0F00 & self.opcode) >> 8] == self.V[(0x0F00 & self.opcode) >> 4]) {
+                self.pc += 4;
+            } else {
+                self.pc += 2;
+            }
+        },
+        // 6XNN
+        // Set register X to number NN
+        0x6000 => {
+            self.V[x] = self.opcode & 0x00FF;
+            self.pc += 2;
+        },
+        // 7XNN
+        // Add number NN to register X
+        0x7000 => {
+            self.V[x] += self.opcode & 0x00FF;
+            self.pc += 2;
+        },
+        0x8000 => {
+            // switch statement
+        },
+        // 9XY0
+        // Skip next instruction if registers are not equal
+        0x9000 => {
+            if (self.V[(0x0F00 & self.opcode) >> 8] != self.V[(0x0F00 & self.opcode) >> 4]) {
+                self.pc += 4;
+            } else {
+                self.pc += 2;
+            }
+        },
+        // ANNN
+        // Add NNN at ANNN to I register
+        0xA000 => {
+            self.I = self.opcode & 0x0FFF;
+            self.pc += 2;
+        },
+        // Unimplemented
+        0xB000 => {
+            // Something idk
+        },
+        // CXNN
+        // Random
+        0xC000 => {
+            // self.V[x] = (self.opcode & 0x00FF) & (random something zig)
+        },
+        // DXYN
+        // Display
+        0xD000 => {
+            // Grab the coordinates in registers X and Y
+            const xPos: usize = self.V[x];
+            const yPos: usize = self.V[y];
+            // Set VF to 0
+            self.V[0xF] = 0;
+            // For N rows
+            for (0..(self.opcode & 0x000F)) |row| {
+                // Get the Nth (row) byte of sprite data
+                const sprite_row = self.memory[self.I + row];
+                // 0x80 is 10000000 which is the first in the sprite row
+                for (0..8) |bit| {
+                    // Don't know if the intcast thing is necessary TODO test
+                    if (sprite_row & (0x80 >> @intCast(bit)) != 0) {
+                        const px = (xPos + bit) % 64;
+                        const py = (yPos + row) % 32;
 
-                            const index = px * 64 + py;
-                            if (self.gfx[index] == 1) {
-                                self.V[0xF] = 1;
-                            }
-                            // XOR the pixel
-                            self.gfx[index] ^= 1;
+                        const index = px * 64 + py;
+                        if (self.gfx[index] == 1) {
+                            self.V[0xF] = 1;
                         }
+                        // XOR the pixel
+                        self.gfx[index] ^= 1;
                     }
                 }
-                self.pc += 2;
-            },
-            0xE000 => {
-                // Switch here
-            },
-            0xF000 => {
-                // Switch here
-            },
+            }
+            self.pc += 2;
+        },
+        0xE000 => {
+            // Switch here
+        },
+        0xF000 => {
+            // Switch here
+        },
+    }
+}
+pub fn draw_graphics(self: *Self, renderer: c.SDL_Renderer) void {
+    // Draw the graphics bruh
+    // Clear screen to black
+    c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    c.SDL_RenderClear(renderer);
+
+    // Draw white pixels
+    c.SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+    for (0..32) |y| {
+        for (0..64) |x| {
+            if (self.gfx[y * 64 + x] != 0) {
+                const rect: c.SDL_FRect = .{ x * 10, y * 10, 10, 10 };
+                c.SDL_RenderFillRect(renderer, &rect);
+            }
         }
     }
-    pub fn draw_graphics(self: *Self, Renderer: SDL_Renderer) void {}
-};
+
+    // Present the frame
+    c.SDL_RenderPresent(renderer);
+}
